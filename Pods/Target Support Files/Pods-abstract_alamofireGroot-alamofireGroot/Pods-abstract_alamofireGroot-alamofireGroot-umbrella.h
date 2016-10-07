5b559bbbdf3a411f0e2a12e3d9d4f846f13a4dda
Manuel G-Estañ <manuel.garcia.estan@gmail.com>
2016-10-07 12:09:21 +0200

    Rename targets

---
+++ Pods/Target Support Files/Pods-abstract_alamofireGroot-alamofireGroot/Pods-abstract_alamofireGroot-alamofireGroot-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_abstract_alamofireGroot_alamofireGrootVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_abstract_alamofireGroot_alamofireGrootVersionString[];
+
---
+++ alamofireGrootTests/serializers/SerializersTests.swift
@@ -0,0 +1,23 @@
+//
+//  SerializersTests.swift
+//  alamofire+groot
+//
+//  Created by Manuel García-Estañ on 7/10/16.
+//  Copyright © 2016 ManueGE. All rights reserved.
+//
+
+import XCTest
+@testable import alamofireGroot
+
+class SerializersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    
+}
