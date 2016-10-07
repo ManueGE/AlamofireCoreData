9921fc2b15b056c9d1c6a6a3e237f7598c119d3f
Manuel G-Estañ <manuel.garcia.estan@gmail.com>
2016-10-07 15:48:07 +0200

    Add ManyInsertable protocol

--- AlamofireGroot/source/insertable/Insertable.swift
+++ AlamofireGroot/source/insertable/Insertable.swift
@@ -17,7 +17,7 @@ public enum InsertError: Error {
     case invalidJSON(Any)
 }
 
-/// Objects that can be inserted into a `NSManagedObjectContext`.
+/// Objects that can be inserted into a `NSManagedObjectContext` from a JSON object
 public protocol Insertable {
     
     /// Insert an object of the receiver type in the given context using the received JSON
@@ -30,3 +30,29 @@ public protocol Insertable {
     /// - returns: The inserted object
     static func insert(from json: Any, in context: NSManagedObjectContext) throws -> Self
 }
+
+/// Objects that can be bulk-inserted into a `NSManagedObjectContext` from a JSON array
+public protocol ManyInsertable {
+    
+    /// Insert an array object of the receiver type in the given context using the received JSON array
+    ///
+    /// - parameter json:    The JSON used to insert the objects
+    /// - parameter context: The context where the objects will be inserted
+    ///
+    /// - throws: An `InsertError` If the JSON can't be inserted.
+    ///
+    /// - returns: The inserted objects. Even tough they are `Any`, they must be the same Type as the receiver
+    static func insertMany(from json: Any, in context: NSManagedObjectContext) throws -> [Any]
+}
+
+/// When a ManyInsertable is a Insertable too, the default behaviour is return an array built by calling `insert` to any element 
+extension ManyInsertable where Self: Insertable {
+    public static func insertMany(from json: Any, in context: NSManagedObjectContext) throws -> [Any] {
+        guard let array = json as? JSONArray else {
+            throw InsertError.invalidJSON(json)
+        }
+        
+        return array.flatMap { try? insert(from: $0, in: context) }
+    }
+}
+
