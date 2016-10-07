d0223146bfae4253e7559d79099589c13a61ee49
Manuel G-Estañ <manuel.garcia.estan@gmail.com>
2016-10-07 11:48:33 +0200

    Add cocoapods

---
+++ Pods/Target Support Files/Groot/Groot-umbrella.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+#import "Groot.h"
+#import "GRTError.h"
+#import "GRTJSONSerialization.h"
+#import "GRTManagedStore.h"
+#import "NSValueTransformer+Groot.h"
+
+FOUNDATION_EXPORT double GrootVersionNumber;
+FOUNDATION_EXPORT const unsigned char GrootVersionString[];
+
---
+++ Pods/Target Support Files/Pods-alamofire+groot/Pods-alamofire+groot-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_alamofire_grootVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_alamofire_grootVersionString[];
+
