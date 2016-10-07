dd944ecaa33c35ea182a6446d6e10c72de28d8c4
Manuel G-Estañ <manuel.garcia.estan@gmail.com>
2016-10-07 17:45:02 +0200

    Test single wrapper insert

--- AlamofireGrootTests/WrapperTests.swift
+++ AlamofireGrootTests/WrapperTests.swift
@@ -11,21 +11,32 @@ import XCTest
 import CoreData
 import Alamofire
 
-
 class WrapperTests: InsertableTests {
     
     // MARK: Without serializer
-    func testSerializeSingleManagedObject() {
+    func testSerializeSingleWrapper() {
         
         // given
         let responseArrived = self.expectation(description: "response of async request has arrived")
-        var receivedObject: User?
-        let expectedJSON: [String: Any] = ["id": 10, "name": "manueGE"]
+        var receivedObject: LoginResponse?
+        let expectedJSON: [String: Any] = [
+            "token": "my token",
+            "refresh_token": "my refresh token",
+            "validity": "18/11/1983",
+            "page": 5,
+            
+            "user": ["id": 10, "name": "manueGE"],
+            
+            "friends": [
+                ["id": 11, "name": "mila"],
+                ["id": 12, "name": "anaE"],
+            ]
+        ]
         
         // when
         stubSuccess(with: expectedJSON)
         Alamofire.request(apiURL)
-            .responseInsert(context: persistentContainer.viewContext, type: User.self) { response in
+            .responseInsert(context: persistentContainer.viewContext, type: LoginResponse.self) { response in
                 switch response.result {
                 case let .success(user):
                     receivedObject = user
@@ -38,12 +49,31 @@ class WrapperTests: InsertableTests {
         // then
         self.waitForExpectations(timeout: 2) { err in
             XCTAssertNotNil(receivedObject, "Received data should not be nil")
-            XCTAssertEqual(receivedObject?.id, 10, "property does not match")
-            XCTAssertEqual(receivedObject?.name, "manueGE", "property does not match")
-            XCTAssertEqual(receivedObject?.managedObjectContext, self.persistentContainer.viewContext, "property does not match")
+            XCTAssertEqual(receivedObject?.token, "my token", "property does not match")
+            XCTAssertEqual(receivedObject?.refreshToken, "my refresh token", "property does not match")
+            XCTAssertEqual(receivedObject?.page, 5, "property does not match")
+            
+            let date = DateComponents(calendar: .current,
+                                      year: 1983,
+                                      month: 11,
+                                      day: 18).date
+            
+            XCTAssertEqual(receivedObject?.validity, date, "Received data should not be nil")
+            
+            let user = receivedObject?.user
+            XCTAssertEqual(user?.id, 10, "Received data should not be nil")
+            XCTAssertEqual(user?.name, "manueGE", "property does not match")
+            XCTAssertEqual(user?.managedObjectContext, self.persistentContainer.viewContext, "property does not match")
+            
+            let friends = receivedObject?.friends
+            XCTAssertEqual(friends?.count, 2, "Received data should not be nil")
+            XCTAssertEqual(friends?.first?.id, 11, "Received data should not be nil")
+            XCTAssertEqual(friends?.first?.name, "mila", "Received data should not be nil")
+            XCTAssertEqual(friends?.first?.managedObjectContext, self.persistentContainer.viewContext, "property does not match")
         }
     }
     
+    /*
     // MARK: Serializer with transformer
     func testSerializingSingleManagedObjectWithTransformers() {
         // given
@@ -176,4 +206,5 @@ class WrapperTests: InsertableTests {
             XCTAssertNotNil(error, "error should not be nil")
         }
     }
+ */
 }
