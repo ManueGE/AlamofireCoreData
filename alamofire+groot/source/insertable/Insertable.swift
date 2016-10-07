003137365dcf0642e641f8d47713ce8f1ab0ee57
Manuel G-Estañ <manuel.garcia.estan@gmail.com>
2016-10-07 12:54:25 +0200

    Add documentation to Insertable

--- alamofire+groot/source/insertable/Insertable.swift
+++ alamofire+groot/source/insertable/Insertable.swift
@@ -10,6 +10,16 @@ import Foundation
 import CoreData
 import Groot
 
+/// Objects that can be inserted into a `NSManagedObjectContext`.
 public protocol Insertable {
+    
+    /// Insert an object of the receiver type in the given context using the received JSON
+    ///
+    /// - parameter json:    The JSON used to insert the object
+    /// - parameter context: The context where the object will be inserted
+    ///
+    /// - throws: If the JSON can't be inserted.
+    ///
+    /// - returns: The inserted object
     static func insert(from json: Any, in context: NSManagedObjectContext) throws -> Self
 }
