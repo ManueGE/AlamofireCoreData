23eb8abdd3bb1780253d86235d82cbe06906df2a
Manuel G-Estañ <manuel.garcia.estan@gmail.com>
2016-10-07 12:51:43 +0200

    Add documentation to Alamofire extension

--- alamofire+groot/source/alamofire+coredata/Alamofire+CoreData.swift
+++ alamofire+groot/source/alamofire+coredata/Alamofire+CoreData.swift
@@ -12,29 +12,53 @@ import Groot
 
 extension DataResponseSerializer {
     
-    public init<ParentValue>(parent: DataResponseSerializer<ParentValue>, serializeResponse: @escaping (Result<ParentValue>) -> Result<Value>) {
+    /// Createa a new `DataResponseSerializer` which serialize the response in two steps:
+    /// - first, it serialize the response using the serializer sent in the parent parameter
+    /// - second, take the `Result` returned by the parent and process it using the given transformer
+    /// - parameter parent:             The serializer used in the first serialization
+    /// - parameter transformer:        The block used for the second serialization
+    ///
+    /// - returns: a new instance of the serializer
+    public init<ParentValue>(parent: DataResponseSerializer<ParentValue>, transformer: @escaping (Result<ParentValue>) -> Result<Value>) {
         
         self.init(serializeResponse: { request, response, data, error -> Result<Value> in
             let initialResponse = parent.serializeResponse(request, response, data, error)
-            return serializeResponse(initialResponse)
+            return transformer(initialResponse)
         })
     }
 }
 
 extension DataRequest {
     
+    /// Initialize a serializer built in two steps:
+    /// - The first step serializes the response to get a `JSON`.
+    /// - The second step transform the previous `JSON` using the given transformer
+    ///
+    /// - parameter options:     The JSON serialization reading options. Default is `.allowFragments`
+    /// - parameter transformer: The transformer used to proccess the default `JSON`
+    ///
+    /// - returns: the new serializer
     public static func jsonTransformerSerializer(
-        options: JSONSerialization.ReadingOptions = [],
+        options: JSONSerialization.ReadingOptions = .allowFragments,
         transformer: @escaping ((Result<Any>) -> Result<Any>)
         ) -> DataResponseSerializer<Any> {
         
         let parentSerializer = DataRequest.jsonResponseSerializer(options: options)
-        return DataResponseSerializer(parent: parentSerializer, serializeResponse: transformer)
+        return DataResponseSerializer(parent: parentSerializer, transformer: transformer)
     }
     
-    private class func responseInsertSerializer<T: Insertable>(
+    /// Creates a response serializer that returns a `Insertable` object result type constructed from the response data using. 
+    /// The `Insertable` will be inserted in the given context before being returned.
+    ///
+    /// - parameter context:        The `NSManagedObjectContext` where the `Insertable` will be inserted
+    /// - parameter type:              The `Insertable` type that will be used in the serialization
+    /// - parameter jsonSerializer: A `DataResultSerializer` which must return the JSON which will be used to perform the insert. Default is a `DataRequest.jsonResponseSerializer()`
+    ///
+    /// - returns: An `Insertable` object response serializer.
+    public class func responseInsertSerializer<T: Insertable>(
         context: NSManagedObjectContext,
-        jsonSerializer: DataResponseSerializer<Any>
+        type: T.Type,
+        jsonSerializer: DataResponseSerializer<Any> = DataRequest.jsonResponseSerializer()
         ) -> DataResponseSerializer<T> {
         
         return DataResponseSerializer(parent: jsonSerializer) { (result) -> Result<T> in
@@ -54,6 +78,15 @@ extension DataRequest {
         }
     }
     
+    /// Adds a handler to be called once the request has finished.
+    ///
+    /// - parameter queue:             The queue on which the completion handler is dispatched.
+    /// - parameter jsonSerializer:    The se
+    /// - parameter context:           A `DataResultSerializer` which must return the JSON which will be used to perform the insert. Default is a `DataRequest.jsonResponseSerializer()`
+    /// - parameter type:              The `Insertable` type that will be used in the serialization
+    /// - parameter completionHandler: The code to be executed once the request has finished.
+    ///
+    /// - returns: the request
     @discardableResult
     public func responseInsert<T: Insertable>(
         queue: DispatchQueue? = nil,
@@ -63,7 +96,12 @@ extension DataRequest {
         completionHandler: @escaping (DataResponse<T>) -> Void)
         -> Self
     {
-        let serializer: DataResponseSerializer<T> = DataRequest.responseInsertSerializer(context: context, jsonSerializer: jsonSerializer)
+        let serializer = DataRequest.responseInsertSerializer(
+            context: context,
+            type: T.self,
+            jsonSerializer: jsonSerializer
+        )
+        
         return response(
             queue: queue,
             responseSerializer: serializer,
